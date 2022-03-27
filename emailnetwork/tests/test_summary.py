4373a29bda6dd250673417ab85eed3392135d77a
Karina Widyastuti <karina.widyastuti@onebyone.com.sg>
2022-03-27 15:16:49 +0700

    add new test for test_extract line #89 - #119 and test_summary line #50 - #66

--- emailnetwork/tests/test_summary.py
+++ emailnetwork/tests/test_summary.py
@@ -1,12 +1,15 @@
 import os
-from unittest import TestCase
+import matplotlib.pyplot as plt
+from unittest import TestCase, mock
 from collections import Counter
 
 from emailnetwork.extract import MBoxReader
 from emailnetwork.summary import DomainSummary, IncomingOutgoingSummary
 from emailnetwork.header import HeaderCounter
 
-MBOX_PATH = f'{os.path.dirname(__file__)}/test.mbox'
+import emailnetwork.summary as summary
+
+MBOX_PATH = f"{os.path.dirname(__file__)}/test.mbox"
 
 
 class TestSummary(TestCase):
@@ -23,30 +26,41 @@ class TestSummary(TestCase):
     def test_summary_instance(self):
         self.assertIsInstance(self.domain_summary, DomainSummary)
         self.assertIsInstance(self.domain_summary.summary, Counter)
-        self.assertIsInstance(
-            self.incoming_outgoing_summary, IncomingOutgoingSummary)
-        self.assertIsInstance(
-            self.incoming_outgoing_summary.summary, dict)
+        self.assertIsInstance(self.incoming_outgoing_summary, IncomingOutgoingSummary)
+        self.assertIsInstance(self.incoming_outgoing_summary.summary, dict)
 
     def test_one_summary(self):
         for summary in self.domain_summary.summary:
             self.assertIsInstance(summary, str)
-            self.assertIsInstance(
-                self.domain_summary.summary[summary], int)
+            self.assertIsInstance(self.domain_summary.summary[summary], int)
             self.assertGreater(self.domain_summary.summary[summary], 0)
 
         for summary in self.incoming_outgoing_summary.summary:
             self.assertIsInstance(summary, str)
-            self.assertIsInstance(
-                self.incoming_outgoing_summary.summary[summary], dict)
+            self.assertIsInstance(self.incoming_outgoing_summary.summary[summary], dict)
             for keys in self.incoming_outgoing_summary.summary[summary]:
-                self.assertIn(keys, ('Incoming', 'Outgoing'))
+                self.assertIn(keys, ("Incoming", "Outgoing"))
                 self.assertIsInstance(
-                    self.incoming_outgoing_summary.summary[summary][keys], int)
+                    self.incoming_outgoing_summary.summary[summary][keys], int
+                )
 
     def test_header(self):
         self.assertIsInstance(self.headers, HeaderCounter)
 
-    
-
-
+    @mock.patch(f"{__name__}.summary.plt")
+    def test_mock_plot(self, mock_plt):
+        reader = MBoxReader(
+            "/Users/karinawidyastuti/Documents/Supertype/UnitTesting/emailnetwork/emailnetwork/tests/test.mbox"
+        )
+        ds = DomainSummary(reader=reader)
+        ds.plot()
+        mock_plt.title.assert_called_once_with(
+            "Sender's Domain Occurences",
+            fontdict={
+                "fontname": "Helvetica",
+                "color": "k",
+                "fontweight": "bold",
+                "fontsize": 12,
+            },
+        )
+        assert mock_plt.figure.called
